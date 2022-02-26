1a3cf95a3a46908400f5692ffc2b31e23c2794c9
Akhil <akhilr37@gmail.com>
2022-02-26 05:09:49 +0000

    API-1 completed and 6 test cases passed

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const sqlite3 = require("sqlite3");
 const { open } = require("sqlite");
 const path = require("path");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const DBpath = path.join(__dirname, "twitterClone.db");
 let db = null;
 const app = express();
@@ -26,13 +28,43 @@ const serverInitilization = async () => {
 
 serverInitilization();
 
-app.get("/", async (request, response) => {
-  const check = `
+// UTILITIES
+
+const passwordCheck = (wordstring) => {
+  if (wordstring.length < 6) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+// 1) POST API: /register/
+
+app.post("/register/", async (request, response) => {
+  const { username, password, name, gender } = request.body;
+  const encryptedPassword = await bcrypt.hash(request.body.password, 10);
+  const userCheckingQuery = `
     SELECT
     *
     FROM
-    user;`;
-  const forstresponse = await db.get(check);
-  response.send(forstresponse);
+    user
+    WHERE username = "${username}";`;
+  const userPostingQuery = `
+  INSERT INTO
+    user(username, password, name, gender)
+  VALUES("${username}","${encryptedPassword}","${name}","${gender}");`;
+  const userExistance = await db.get(userCheckingQuery);
+  if (userExistance !== undefined) {
+    response.status(400);
+    response.send("User already exists");
+  } else {
+    if (passwordCheck(password)) {
+      await db.run(userPostingQuery);
+      response.send("User created successfully");
+    } else {
+      response.status(400);
+      response.send("Password is too short");
+    }
+  }
 });
-console.log("akhil");
+module.exports = app;
